5af8304f3ee0513102337735b23de85589a0abbd
R Makosz <radoslaw@taureca.com>
2020-05-18 09:56:10 +0200

    Changed the color palette and fixed some css issues

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
     <div className="App">
       <h3 className="App__title">Dodawaj, usuwaj i sortuj zdjęcia do woli!</h3>
       <ImageUploader requestURL={'http://192.168.1.2:4000/photos'} requestDataName={'photos'}  />
+
+      <footer className="App__footer">
+        Autor R Makosz, <a href="">Zobacz kod na github</a>
+      </footer>
     </div>
   );
 }
